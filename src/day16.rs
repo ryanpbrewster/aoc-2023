e5cdfecfd9d8564ee5f8bf201e928d2cc4c2f54b
Ryan P. Brewster <RyanPBrewster@gmail.com>
2023-12-16 13:50:35 -0500

    optimize day16

--- src/day16.rs
+++ src/day16.rs
@@ -1,4 +1,4 @@
-use crate::grid::{Direction, Grid, Position};
+use crate::grid::{Dimensions, Direction, Grid, Position};
 
 /*
 --- Day 16: The Floor Will Be Lava ---
@@ -104,10 +104,7 @@ use nom::{
     sequence::delimited,
     IResult,
 };
-use std::{
-    collections::HashSet,
-    fmt::{Debug, Write},
-};
+use std::fmt::{Debug, Write};
 pub fn part1(input: &str) -> anyhow::Result<usize> {
     let grid = parse_input(input)?;
     Ok(count_energized(&grid, (Position(0, 0), Direction::Right)))
@@ -125,47 +122,84 @@ pub fn part2(input: &str) -> anyhow::Result<usize> {
     Ok(up.chain(down).chain(left).chain(right).max().unwrap_or(0))
 }
 
-fn count_energized(grid: &Grid<Cell>, enter: (Position, Direction)) -> usize {
-    let mut vis: HashSet<(Position, Direction)> = HashSet::new();
-    let mut stack: Vec<(Position, Direction)> = vec![enter];
-    while let Some(e) = stack.pop() {
-        let (pos @ Position(i, j), dir) = e;
-        let Some(ch) = grid.get(i, j) else { continue };
-        if !vis.insert(e) {
-            continue;
+fn count_energized(grid: &Grid<Cell>, (pos, dir): (Position, Direction)) -> usize {
+    fn explore(grid: &Grid<Cell>, vis: &mut PosDirSet, pos: Position, dir: Direction) {
+        let Position(i, j) = pos;
+        let Some(&ch) = grid.get(i, j) else { return };
+        if !vis.insert(pos, dir) {
+            return;
         }
-        let next_dirs = match ch {
-            Cell::Ground => vec![dir],
+        match ch {
+            Cell::Ground => explore(grid, vis, pos.step(dir), dir),
             Cell::MirrorUp /* aka '/' */ => match dir {
-                Direction::Up => vec![Direction::Right],
-                Direction::Down => vec![Direction::Left],
-                Direction::Left => vec![Direction::Down],
-                Direction::Right => vec![Direction::Up],
+                Direction::Up => explore(grid, vis, pos.step(Direction::Right), Direction::Right),
+                Direction::Down => explore(grid, vis, pos.step(Direction::Left), Direction::Left),
+                Direction::Left => explore(grid, vis, pos.step(Direction::Down), Direction::Down),
+                Direction::Right => explore(grid, vis, pos.step(Direction::Up), Direction::Up),
             },
             Cell::MirrorDown /* aka '\' */ => match dir {
-                Direction::Up => vec![Direction::Left],
-                Direction::Down => vec![Direction::Right],
-                Direction::Left => vec![Direction::Up],
-                Direction::Right => vec![Direction::Down],
+                Direction::Up => explore(grid, vis, pos.step(Direction::Left), Direction::Left),
+                Direction::Down => explore(grid, vis, pos.step(Direction::Right), Direction::Right),
+                Direction::Left => explore(grid, vis, pos.step(Direction::Up), Direction::Up),
+                Direction::Right => explore(grid, vis, pos.step(Direction::Down), Direction::Down),
             },
             Cell::SplitHoriz => match dir {
-                Direction::Up | Direction::Down => vec![Direction::Left, Direction::Right],
-                Direction::Left | Direction::Right => vec![dir],
+                Direction::Up | Direction::Down => {
+                    explore(grid, vis, pos.step(Direction::Left), Direction::Left);
+                    explore(grid, vis, pos.step(Direction::Right), Direction::Right);
+                }
+                Direction::Left | Direction::Right => explore(grid, vis, pos.step(dir), dir),
             }
             Cell::SplitVert => match dir {
-                Direction::Up | Direction::Down => vec![dir],
-                Direction::Left | Direction::Right => vec![Direction::Up, Direction::Down],
+                Direction::Left | Direction::Right => {
+                    explore(grid, vis, pos.step(Direction::Up), Direction::Up);
+                    explore(grid, vis, pos.step(Direction::Down), Direction::Down);
+                }
+                Direction::Up | Direction::Down => explore(grid, vis, pos.step(dir), dir),
             }
         };
-        for next_dir in next_dirs {
-            stack.push((pos.step(next_dir), next_dir));
+    }
+    let mut vis: PosDirSet = PosDirSet::new(grid.size());
+    explore(grid, &mut vis, pos, dir);
+    vis.count_positions()
+}
+
+// A specialized HashSet<(Position, Direction)> that pre-allocates a Vec<bool> of the right
+// size to handle all expected inputs.
+struct PosDirSet {
+    dims: Dimensions,
+    bits: Vec<bool>,
+}
+impl PosDirSet {
+    fn new(dims: Dimensions) -> Self {
+        Self {
+            bits: vec![false; (4 * dims.height * dims.width) as usize],
+            dims,
         }
     }
-    // Count just the distinct positions we energized
-    vis.into_iter()
-        .map(|(pos, _dir)| pos)
-        .collect::<HashSet<_>>()
-        .len()
+    fn insert(&mut self, Position(i, j): Position, dir: Direction) -> bool {
+        let idx = 4 * (i * self.dims.width + j)
+            + match dir {
+                Direction::Up => 0,
+                Direction::Down => 1,
+                Direction::Left => 2,
+                Direction::Right => 3,
+            };
+        let Some(entry) = self.bits.get_mut(idx as usize) else {
+            return false;
+        };
+        if !*entry {
+            *entry = true;
+            return true;
+        }
+        false
+    }
+    fn count_positions(&self) -> usize {
+        self.bits
+            .chunks(4)
+            .filter(|chunk| chunk.iter().any(|&b| b))
+            .count()
+    }
 }
 
 fn parse_input(input: &str) -> anyhow::Result<Grid<Cell>> {
